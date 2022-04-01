bdc533c10626b1f98f31711063936a7c73760a81
tliang3612 <tliang@csu.fullerton.edu>
2022-04-01 14:19:15 -0700

    changed some files

--- levels/tilesets/powerups.tsx
+++ levels/tilesets/powerups.tsx
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Flower/Mushroom" tilewidth="17" tileheight="17" tilecount="2" columns="0">
+<tileset version="1.8" tiledversion="1.8.2" name="Flower/Mushroom" tilewidth="64" tileheight="64" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="16" height="16" source="../../graphics/powerups/1UP_Mushroom.png"/>
+ <tile id="2">
+  <image width="64" height="64" source="../../graphics/powerups/1UP_Mushroom.png"/>
  </tile>
- <tile id="1">
-  <image width="17" height="17" source="../../graphics/powerups/Fire_Flower.png"/>
+ <tile id="3">
+  <image width="64" height="64" source="../../graphics/powerups/Fire_Flower.png"/>
  </tile>
 </tileset>
